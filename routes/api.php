0ad858609637375caf33c56d556d28438f07bd75
Samir Jabre <samirjabre8@gmail.com>
2025-01-27 21:43:07 +0200

    Add new unique click tracking routes and remove deprecated endpoints in api.php

--- routes/api.php
+++ routes/api.php
@@ -31,16 +31,14 @@ Route::controller(AggregateController::class)->group(function () {
     Route::get('increment_news_clicks', 'increment_News');
     Route::get('increment_most_read_clicks', 'increment_Most_read');
     Route::get('increment_footer_clicks', 'increment_Footer');
+    Route::get('unique_navigation', 'increment_unique_nav_clicks');
+    Route::get('unique_hero', 'increment_unique_hero_clicks');
+    Route::get('unique_news', 'increment_unique_news_clicks');
+    Route::get('unique_most_read', 'increment_unique_most_read_clicks');
+    Route::get('unique_footer' , 'increment_unique_footer_clicks');
+    Route::get('top_news_categories', 'getTopNewsCategories');
+    Route::get('clicks/{period}', 'getClicksByPeriod');
 });
 
-Route::post('/increment-click', [AggregateController::class, 'incrementSectionClick']);
-Route::get('/unique-clicks/{section}', [AggregateController::class, 'getUniqueClicks']);
 Route::get('/get_category', [CategoryController::class, 'getCategories']);
 Route::get('/get_category/{id}', [CategoryController::class, 'getCategory']);
-Route::get('/unique_footer', [AggregateController::class, 'increment_unique_footer_clicks']);
-
-
-
-
-Route::get('/clicks/{period}', [AggregateController::class, 'getClicksByPeriod']);
-Route::get('/top-news-categories', [AggregateController::class, 'getTopNewsCategories']);
